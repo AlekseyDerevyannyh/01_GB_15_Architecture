25d78bf1d778198918f16ca82951258402d4c0c5
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-08-26 18:52:26 +0300

    modify Seminar 01

--- seminar/01/task1/src/store/Order.java
+++ seminar/01/task1/src/store/Order.java
@@ -1,5 +1,32 @@
 package store;
 
 public class Order {
+    private static int counter = 10000;
+    private int id;
+    private Date orderDate;
+    private String address;
+    private String phone;
+    private Buyer buyer;
+    private List<OrderItem> items = new ArrayList<>();
 
+    {
+        id = ++counter;
+    }
+
+    public Order(Date orderDate, String address, String phone, Buyer buyer, List<OrderItem> items) {
+
+        if (buyer == null) {
+            throw new RuntimeException("Покупатель должен существовать.");
+        }
+
+        if (items == null || items.size() == 0) {
+            throw new RuntimeException("Заказ должен содержать минимум одну позицию.");
+        }
+
+        this.orderDate = orderDate;
+        this.address = address;
+        this.phone = phone;
+        this.buyer = buyer;
+        this.items = items;
+    }
 }
