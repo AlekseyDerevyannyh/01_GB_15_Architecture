12372b48e1591e834cf78994ca68b909fb07a34d
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-08-31 15:39:46 +0300

    add Seminar 2 task1 - templatemethod

---
+++ seminar/02/task1/src/templatemethod/LogEntry.java
@@ -0,0 +1,16 @@
+package templatemethod;
+
+/**
+ * Запись лога
+ */
+public class LogEntry {
+    private String text;
+
+    public LogEntry(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
