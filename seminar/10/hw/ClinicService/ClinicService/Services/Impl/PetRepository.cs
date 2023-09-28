aa5476ff5118e9c9af053cdb8f2e971b0fda7212
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-09-28 12:59:25 +0300

    init HW 10

---
+++ seminar/10/hw/ClinicService/ClinicService/Services/Impl/PetRepository.cs
@@ -0,0 +1,56 @@
+﻿using ClinicService.Models;
+using Microsoft.Data.Sqlite;
+
+namespace ClinicService.Services.Impl
+{
+    public class PetRepository : IPetRepository
+    {
+
+        private const string connectionString = "Data Source = clinic.db;";
+
+        public int Create(Pet item)
+        {
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "INSERT INTO pets(ClientId, Name, Birthday) VALUES(@CleintId, @Name, @Birthday)";
+                command.Parameters.AddWithValue("@CleintId", item.ClientId);
+                command.Parameters.AddWithValue("@Name", item.Name);
+                command.Parameters.AddWithValue("@Birthday", item.Birthday.Ticks);
+                command.Prepare();
+            }
+        }
+
+        public int Update(Pet item)
+        {
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "UPDATE pets SET PetId = @PetId, ClientId = @ClientId, Name = @Name, Birthday = @Birthday";
+                command.Parameters.AddWithValue("@PetId", item.PetId);
+                command.Parameters.AddWithValue("@ClientId", item.ClientId);
+                command.Parameters.AddWithValue("@Name", item.Name);
+                command.Parameters.AddWithValue("@Birthday", item.Birthday.Ticks);
+                command.Prepare();
+                return command.ExecuteNonQuery();
+            }
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pet> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pet GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
