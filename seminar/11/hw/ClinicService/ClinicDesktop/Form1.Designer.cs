32d8c4f7a7f44130d27dd07e38da258fc70466ee
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-10-02 15:05:27 +0300

    complete HW 11

--- seminar/11/hw/ClinicService/ClinicDesktop/Form1.Designer.cs
+++ seminar/11/hw/ClinicService/ClinicDesktop/Form1.Designer.cs
@@ -29,39 +29,51 @@
         private void InitializeComponent()
         {
             listViewClients = new ListView();
-            btnUpdateClients = new Button();
             columnHeaderId = new ColumnHeader();
             columnHeaderSurName = new ColumnHeader();
             columnHeaderFirstName = new ColumnHeader();
             columnHeaderPatronymic = new ColumnHeader();
+            btnUpdateClients = new Button();
+            groupBoxAddClient = new GroupBox();
+            buttonAddClient = new Button();
+            dateTimePickerBirthday = new DateTimePicker();
+            labelBirthday = new Label();
+            labelPatronymic = new Label();
+            textBoxPatronymic = new TextBox();
+            labelFirstName = new Label();
+            textBoxFirstName = new TextBox();
+            labelSurName = new Label();
+            textBoxSurName = new TextBox();
+            labelDocument = new Label();
+            textBoxDocument = new TextBox();
+            listViewPets = new ListView();
+            columnHeaderPetId = new ColumnHeader();
+            columnHeaderPetClientId = new ColumnHeader();
+            columnHeaderPetName = new ColumnHeader();
+            columnHeaderPetBirthday = new ColumnHeader();
+            btnUpdatePets = new Button();
+            columnHeaderDocument = new ColumnHeader();
+            columnHeaderClientBirthday = new ColumnHeader();
+            groupBoxAddClient.SuspendLayout();
             SuspendLayout();
             // 
             // listViewClients
             // 
-            listViewClients.Columns.AddRange(new ColumnHeader[] { columnHeaderId, columnHeaderSurName, columnHeaderFirstName, columnHeaderPatronymic });
+            listViewClients.Columns.AddRange(new ColumnHeader[] { columnHeaderId, columnHeaderDocument, columnHeaderSurName, columnHeaderFirstName, columnHeaderPatronymic, columnHeaderClientBirthday });
             listViewClients.FullRowSelect = true;
             listViewClients.GridLines = true;
             listViewClients.Location = new Point(12, 12);
             listViewClients.MultiSelect = false;
             listViewClients.Name = "listViewClients";
-            listViewClients.Size = new Size(507, 397);
+            listViewClients.Size = new Size(677, 204);
             listViewClients.TabIndex = 1;
             listViewClients.UseCompatibleStateImageBehavior = false;
             listViewClients.View = View.Details;
             // 
-            // btnUpdateClients
-            // 
-            btnUpdateClients.Location = new Point(12, 415);
-            btnUpdateClients.Name = "btnUpdateClients";
-            btnUpdateClients.Size = new Size(111, 23);
-            btnUpdateClients.TabIndex = 2;
-            btnUpdateClients.Text = "Update Clients";
-            btnUpdateClients.UseVisualStyleBackColor = true;
-            btnUpdateClients.Click += btnUpdateClients_Click;
-            // 
             // columnHeaderId
             // 
             columnHeaderId.Text = "#";
+            columnHeaderId.Width = 40;
             // 
             // columnHeaderSurName
             // 
@@ -78,16 +90,194 @@
             columnHeaderPatronymic.Text = "Patronymic";
             columnHeaderPatronymic.Width = 150;
             // 
+            // btnUpdateClients
+            // 
+            btnUpdateClients.Location = new Point(12, 222);
+            btnUpdateClients.Name = "btnUpdateClients";
+            btnUpdateClients.Size = new Size(111, 23);
+            btnUpdateClients.TabIndex = 2;
+            btnUpdateClients.Text = "View Clients";
+            btnUpdateClients.UseVisualStyleBackColor = true;
+            btnUpdateClients.Click += btnUpdateClients_Click;
+            // 
+            // groupBoxAddClient
+            // 
+            groupBoxAddClient.Controls.Add(buttonAddClient);
+            groupBoxAddClient.Controls.Add(dateTimePickerBirthday);
+            groupBoxAddClient.Controls.Add(labelBirthday);
+            groupBoxAddClient.Controls.Add(labelPatronymic);
+            groupBoxAddClient.Controls.Add(textBoxPatronymic);
+            groupBoxAddClient.Controls.Add(labelFirstName);
+            groupBoxAddClient.Controls.Add(textBoxFirstName);
+            groupBoxAddClient.Controls.Add(labelSurName);
+            groupBoxAddClient.Controls.Add(textBoxSurName);
+            groupBoxAddClient.Controls.Add(labelDocument);
+            groupBoxAddClient.Controls.Add(textBoxDocument);
+            groupBoxAddClient.Location = new Point(12, 251);
+            groupBoxAddClient.Name = "groupBoxAddClient";
+            groupBoxAddClient.Size = new Size(314, 220);
+            groupBoxAddClient.TabIndex = 3;
+            groupBoxAddClient.TabStop = false;
+            groupBoxAddClient.Text = "Add new client";
+            // 
+            // buttonAddClient
+            // 
+            buttonAddClient.Location = new Point(16, 181);
+            buttonAddClient.Name = "buttonAddClient";
+            buttonAddClient.Size = new Size(95, 23);
+            buttonAddClient.TabIndex = 11;
+            buttonAddClient.Text = "Add new client";
+            buttonAddClient.UseVisualStyleBackColor = true;
+            buttonAddClient.Click += buttonAddClient_Click;
+            // 
+            // dateTimePickerBirthday
+            // 
+            dateTimePickerBirthday.Location = new Point(89, 138);
+            dateTimePickerBirthday.Name = "dateTimePickerBirthday";
+            dateTimePickerBirthday.Size = new Size(200, 23);
+            dateTimePickerBirthday.TabIndex = 10;
+            // 
+            // labelBirthday
+            // 
+            labelBirthday.AutoSize = true;
+            labelBirthday.Location = new Point(16, 141);
+            labelBirthday.Name = "labelBirthday";
+            labelBirthday.Size = new Size(51, 15);
+            labelBirthday.TabIndex = 9;
+            labelBirthday.Text = "Birthday";
+            // 
+            // labelPatronymic
+            // 
+            labelPatronymic.AutoSize = true;
+            labelPatronymic.Location = new Point(16, 112);
+            labelPatronymic.Name = "labelPatronymic";
+            labelPatronymic.Size = new Size(68, 15);
+            labelPatronymic.TabIndex = 7;
+            labelPatronymic.Text = "Patronymic";
+            // 
+            // textBoxPatronymic
+            // 
+            textBoxPatronymic.Location = new Point(89, 109);
+            textBoxPatronymic.Name = "textBoxPatronymic";
+            textBoxPatronymic.Size = new Size(200, 23);
+            textBoxPatronymic.TabIndex = 6;
+            // 
+            // labelFirstName
+            // 
+            labelFirstName.AutoSize = true;
+            labelFirstName.Location = new Point(16, 83);
+            labelFirstName.Name = "labelFirstName";
+            labelFirstName.Size = new Size(61, 15);
+            labelFirstName.TabIndex = 5;
+            labelFirstName.Text = "FirstName";
+            // 
+            // textBoxFirstName
+            // 
+            textBoxFirstName.Location = new Point(89, 80);
+            textBoxFirstName.Name = "textBoxFirstName";
+            textBoxFirstName.Size = new Size(200, 23);
+            textBoxFirstName.TabIndex = 4;
+            // 
+            // labelSurName
+            // 
+            labelSurName.AutoSize = true;
+            labelSurName.Location = new Point(16, 54);
+            labelSurName.Name = "labelSurName";
+            labelSurName.Size = new Size(56, 15);
+            labelSurName.TabIndex = 3;
+            labelSurName.Text = "SurName";
+            // 
+            // textBoxSurName
+            // 
+            textBoxSurName.Location = new Point(89, 51);
+            textBoxSurName.Name = "textBoxSurName";
+            textBoxSurName.Size = new Size(200, 23);
+            textBoxSurName.TabIndex = 2;
+            // 
+            // labelDocument
+            // 
+            labelDocument.AutoSize = true;
+            labelDocument.Location = new Point(16, 25);
+            labelDocument.Name = "labelDocument";
+            labelDocument.Size = new Size(63, 15);
+            labelDocument.TabIndex = 1;
+            labelDocument.Text = "Document";
+            // 
+            // textBoxDocument
+            // 
+            textBoxDocument.Location = new Point(89, 22);
+            textBoxDocument.Name = "textBoxDocument";
+            textBoxDocument.Size = new Size(200, 23);
+            textBoxDocument.TabIndex = 0;
+            // 
+            // listViewPets
+            // 
+            listViewPets.Columns.AddRange(new ColumnHeader[] { columnHeaderPetId, columnHeaderPetClientId, columnHeaderPetName, columnHeaderPetBirthday });
+            listViewPets.FullRowSelect = true;
+            listViewPets.GridLines = true;
+            listViewPets.Location = new Point(346, 260);
+            listViewPets.MultiSelect = false;
+            listViewPets.Name = "listViewPets";
+            listViewPets.Size = new Size(343, 152);
+            listViewPets.TabIndex = 4;
+            listViewPets.UseCompatibleStateImageBehavior = false;
+            listViewPets.View = View.Details;
+            // 
+            // columnHeaderPetId
+            // 
+            columnHeaderPetId.Text = "#";
+            columnHeaderPetId.Width = 40;
+            // 
+            // columnHeaderPetClientId
+            // 
+            columnHeaderPetClientId.Text = "ClientId";
+            // 
+            // columnHeaderPetName
+            // 
+            columnHeaderPetName.Text = "Name";
+            columnHeaderPetName.Width = 130;
+            // 
+            // columnHeaderPetBirthday
+            // 
+            columnHeaderPetBirthday.Text = "Birthday";
+            columnHeaderPetBirthday.Width = 150;
+            // 
+            // btnUpdatePets
+            // 
+            btnUpdatePets.Location = new Point(346, 432);
+            btnUpdatePets.Name = "btnUpdatePets";
+            btnUpdatePets.Size = new Size(87, 23);
+            btnUpdatePets.TabIndex = 5;
+            btnUpdatePets.Text = "View Pets";
+            btnUpdatePets.UseVisualStyleBackColor = true;
+            btnUpdatePets.Click += btnUpdatePets_Click;
+            // 
+            // columnHeaderDocument
+            // 
+            columnHeaderDocument.DisplayIndex = 4;
+            columnHeaderDocument.Text = "Document";
+            columnHeaderDocument.Width = 100;
+            // 
+            // columnHeaderClientBirthday
+            // 
+            columnHeaderClientBirthday.Text = "Birthday";
+            columnHeaderClientBirthday.Width = 86;
+            // 
             // ClinicMainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(534, 450);
+            ClientSize = new Size(700, 477);
+            Controls.Add(btnUpdatePets);
+            Controls.Add(listViewPets);
+            Controls.Add(groupBoxAddClient);
             Controls.Add(btnUpdateClients);
             Controls.Add(listViewClients);
             Name = "ClinicMainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Clinic";
+            groupBoxAddClient.ResumeLayout(false);
+            groupBoxAddClient.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -98,5 +288,31 @@
         private ColumnHeader columnHeaderSurName;
         private ColumnHeader columnHeaderFirstName;
         private ColumnHeader columnHeaderPatronymic;
+        private GroupBox groupBoxAddClient;
+        private TextBox textBoxDocument;
+        private Label labelDocument;
+        private Label labelSurName;
+        private TextBox textBoxSurName;
+        private Label labelBirthday;
+        private TextBox textBox4;
+        private Label labelPatronymic;
+        private TextBox textBoxPatronymic;
+        private Label labelFirstName;
+        private TextBox textBoxFirstName;
+        private DateTimePicker dateTimePickerBirthday;
+        private Button buttonAddClient;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ListView listViewPets;
+        private ColumnHeader columnHeaderPetId;
+        private ColumnHeader columnHeaderPetClientId;
+        private ColumnHeader columnHeaderPetName;
+        private ColumnHeader columnHeaderPetBirthday;
+        private Button btnUpdatePets;
+        private ColumnHeader columnHeaderDocument;
+        private ColumnHeader columnHeaderClientBirthday;
     }
 }
--- seminar/11/hw/ClinicService/ClinicDesktop/Form1.cs
+++ seminar/11/hw/ClinicService/ClinicDesktop/Form1.cs
@@ -1,9 +1,12 @@
 using ClinicServiceNamespace;
+using System.CodeDom;
 
 namespace ClinicDesktop
 {
     public partial class ClinicMainForm : Form
     {
+        private const string CLINIC_SERVICE_ADDRESS = "http://localhost:5076/";
+
         public ClinicMainForm()
         {
             InitializeComponent();
@@ -11,7 +14,7 @@ namespace ClinicDesktop
 
         private void btnUpdateClients_Click(object sender, EventArgs e)
         {
-            ClinicClient clinicClient = new ClinicClient("http://localhost:5076/", new HttpClient());
+            ClinicClient clinicClient = new ClinicClient(CLINIC_SERVICE_ADDRESS, new HttpClient());
             ICollection<Client> clients = clinicClient.ClientGetAllAsync().Result;
 
             listViewClients.Items.Clear();
@@ -19,6 +22,10 @@ namespace ClinicDesktop
             {
                 ListViewItem item = new ListViewItem();
                 item.Text = client.ClientId.ToString();
+                item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                {
+                    Text = client.Document
+                });
                 item.SubItems.Add(new ListViewItem.ListViewSubItem()
                 {
                     Text = client.SurName
@@ -31,7 +38,58 @@ namespace ClinicDesktop
                 {
                     Text = client.Patronymic
                 });
+                item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                {
+                    Text = String.Format("{0}.{1}.{2}", client.Birthday.Day.ToString(), client.Birthday.Month.ToString(), client.Birthday.Year.ToString())
+                });
                 listViewClients.Items.Add(item);
+            }
+        }
+
+        private void buttonAddClient_Click(object sender, EventArgs e)
+        {
+            ClinicClient clinicClient = new ClinicClient(CLINIC_SERVICE_ADDRESS, new HttpClient());
+            //Client client = new Client();
+            clinicClient.ClientCreateAsync(new CreateClientRequest
+            {
+                Document = textBoxDocument.Text,
+                SurName = textBoxSurName.Text,
+                FirstName = textBoxFirstName.Text,
+                Patronymic = textBoxPatronymic.Text,
+                Birthday = dateTimePickerBirthday.Value.Date
+            });
+            MessageBox.Show("Client added");
+
+            textBoxDocument.Text = "";
+            textBoxSurName.Text = "";
+            textBoxFirstName.Text = "";
+            textBoxPatronymic.Text = "";
+            dateTimePickerBirthday.Value = DateTime.Now;
+        }
+
+        private void btnUpdatePets_Click(object sender, EventArgs e)
+        {
+            ClinicClient clinicClient = new ClinicClient(CLINIC_SERVICE_ADDRESS, new HttpClient());
+            ICollection<Pet> pets = clinicClient.PetGetAllAsync().Result;
+
+            listViewPets.Items.Clear();
+            foreach (Pet pet in pets)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = pet.PetId.ToString();
+                item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                {
+                    Text = pet.ClientId.ToString()
+                });
+                item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                {
+                    Text = pet.Name
+                });
+                item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                {
+                    Text = String.Format("{0}.{1}.{2}", pet.Birthday.Day.ToString(), pet.Birthday.Month.ToString(), pet.Birthday.Year.ToString())
+                });
+                listViewPets.Items.Add(item);
             }
         }
     }
