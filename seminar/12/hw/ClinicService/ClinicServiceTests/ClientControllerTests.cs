5c78f6e9a53cb5b26776952dec9023845cbf2ce8
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-10-06 22:57:50 +0300

    modify HW 12

--- seminar/12/hw/ClinicService/ClinicServiceTests/ClientControllerTests.cs
+++ seminar/12/hw/ClinicService/ClinicServiceTests/ClientControllerTests.cs
@@ -72,6 +72,60 @@ namespace ClinicServiceTests
                 repository.Create(It.IsNotNull<Client>()), Times.AtLeastOnce());
         }
 
+        public static readonly object[][] CorrectUpdateClientData =
+        {
+            new object[] {1, new DateTime(1985, 5, 20), "123 1234", "Иванов", "Андрей", "Сергеевич" },
+            new object[] {2, new DateTime(1987, 2, 18), "123 2222", "Петров", "Михаил", "Александрович" },
+            new object[] {3, new DateTime(1979, 1, 22), "123 4321", "Сидовров", "Олег", "Константинович" },
+        };
 
+        [Theory]
+        [MemberData(nameof(CorrectUpdateClientData))]
+        public void UpdateClientTest(int ClientId, DateTime birthday, string document, string surName, string firstName, string patronymic)
+        {
+            _moqClientRepository.Setup(repository =>
+            repository
+                .Update(It.IsNotNull<Client>()))
+                .Returns(1).Verifiable();
+
+            var operationResult = _clientController.Update(new UpdateClientRequest
+            {
+                ClientId = ClientId,
+                Birthday = birthday,
+                Document = document,
+                SurName = surName,
+                FirstName = firstName,
+                Patronymic = patronymic
+            });
+
+            Assert.IsType<OkObjectResult>(operationResult.Result);
+            Assert.IsAssignableFrom<int>(((OkObjectResult)operationResult.Result).Value);
+            _moqClientRepository.Verify(repository =>
+                repository.Update(It.IsNotNull<Client>()), Times.AtLeastOnce());
+        }
+
+        public static readonly object[][] CorrectDeleteClientData =
+        {
+            new object[]{1},
+            new object[]{2},
+            new object[]{3}
+        };
+
+        [Theory]
+        [MemberData(nameof(CorrectDeleteClientData))]
+        public void DeleteClientTest(int ClientId)
+        {
+            _moqClientRepository.Setup(repository =>
+            repository
+                .Delete(It.IsNotNull<int>()))
+                .Returns(1).Verifiable();
+
+            var operationResult = _clientController.Delete(ClientId);
+
+            Assert.IsType<OkObjectResult>(operationResult.Result);
+            Assert.IsAssignableFrom<int>(((OkObjectResult)operationResult.Result).Value);
+            _moqClientRepository.Verify(repository =>
+                repository.Delete(It.IsNotNull<int>()), Times.AtLeastOnce());
+        }
     }
 }
